4bc326af718f9c57af2ba174680cd0f4e7aa366c
Robert Prince <rob23codingbootcamp@gmail.com>
2018-02-21 14:48:22 -0800

    changes

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
-var word;
+var gameWord;
 var allowedGuesses;
 var userGuesses;
+var correctGuesses = [];
+var alreadyUsedLetters = [];
 var wordElement = document.getElementById('word');
 var lettersGuessedElement = document.getElementById('lettersGuessed');
+var lettercountelement = document.getElementById('lettercount')
 
-function initializeGame(){
+function initializeGame() {
     var arrayWords = [
         "frog",
         "jump",
@@ -13,44 +16,57 @@ function initializeGame(){
     ]
 
 
-allowedGuesses = 10;
-alreadyGuessedLetters = [];
-correctGuesses = [];
+    allowedGuesses = 10;
+    alreadyGuessedLetters = [];
+    correctGuesses = [];
 
-var wordIndex = Math.floor(Math.random() * arrayWords.length);
-gameWord = arrayWords[wordIndex];
+    var wordIndex = Math.floor(Math.random() * arrayWords.length);
+    gameWord = arrayWords[wordIndex];
 
-   for (var i = 0; i < gameWord.length; i++) {
-    correctGuesses.push('_');
-   }
-  }
-wordElement.innerHTML = correctGuesses.join('');
- document.getElementById("message").innerHTML = ("To begin playing Hnagman, guess the letters of a computer generated word by pressing any key");
-   function updateGuesses(letter){
-       allowedGuesses --;
-       letterCount.innerHTML = allowedGuesses;
+    for (var i = 0; i < gameWord.length; i++) {
+        correctGuesses.push('_');
+    }
+    wordElement.innerHTML = correctGuesses.join('');
+    console.log(gameWord)
+}
+document.getElementById("message").innerHTML = ("To begin playing Hnagman, guess the letters of a computer generated word by pressing any key");
 
-          for (var i = 0; i < alreadyUsedLetter.length; i++) {
-              if (alreadyUsedLettter[i] === letterGuessed) {
-                  alert('You already used that letter, try again');
-              } 
+function updateGuesses(letterGuessed) {
+    console.log(letterGuessed);
+    // allowedGuesses--;
+    lettercountelement.innerHTML = allowedGuesses;
+
+
+    if (alreadyUsedLettters.indexOf(letterGuessed) === -1) {
+        for (var i = 0; i < gameWord.length; i++) {
+            console.log("hi")
+            if (gameWord[i] === letterGuessed) {
+                correctGuesses[i] = letterGuessed;
             }
-            for (var i = 0; i < gameWord.length; i++) {
-                if (gameWord[i] = letterGuessed) {
-                    correctGuess[i] = letterGuessed
-                }
-            }
-                    wordElement.innerHTML = correctGuesses.join('');
+        }
+        wordElement.innerHTML = correctGuesses.join('');
 
-                    document.onkeyup = function(event) {
-                        var letterGuessed = string.fromCharCode(event.keyCode).toLowerCase();
-                        updateGuesses(letterGuessed);
-                         checkWin();
-                    };
+        // push guessedLetter in to alreadyUsedLetters
 
-                    initializeGame();
-                          
+        // Check to see if guessedLetter isn't inside of correctGuesses array (use indexOf again)
+            // if yes, decrement guessesLeft and write that to the DOM
+        
+        // Check to see if guessesLeft === 0
+            // if yes, you lose, increment losses and write it to the dom
+        
+        // Check for win (if correctGuesses.join('') === pickedWord)
+            // if yes, you win!
 
-              }
-          
-   
+    } else {
+        alert('You already used that letter, try again');
+    }
+
+}
+
+document.onkeyup = function (event) {
+    var letterGuessed = event.key;
+    updateGuesses(letterGuessed);
+    // checkWin();
+};
+
+initializeGame();
